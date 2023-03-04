a31f581e4f5783345daad86c99491471abb85391
marina-hoffmann <marina.hoffmann@aurum.com.br>
2023-03-04 08:29:35 -0300

    exercicio 3 - cria footer

--- plataforma-cursos/src/pages/Home.jsx
+++ plataforma-cursos/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar.jsx"
+import Footer from "../components/Footer.jsx"
 
 function Home() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   )
 }
