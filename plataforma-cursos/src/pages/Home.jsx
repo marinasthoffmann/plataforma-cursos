6060d21183fda692b72d14e0628ec13b248234c1
marina-hoffmann <marina.hoffmann@aurum.com.br>
2023-03-05 19:49:03 -0300

    adiciona mais cursos na lista

--- plataforma-cursos/src/pages/Home.jsx
+++ plataforma-cursos/src/pages/Home.jsx
@@ -34,6 +34,70 @@ const coursesList = [
     category: "Back-end",
     description: "Curso de React com TS",
     duration: 10
+  },
+  {
+    id: 222,
+    imageUrl: "./logo.png",
+    name: "ReactJS",
+    category: "Front-end",
+    description: "Curso de ReactJS",
+    duration: 6
+  },
+  {
+    id: 333,
+    imageUrl: "./logo.png",
+    name: "React com TS",
+    category: "Front-end",
+    description: "Curso de React com TS",
+    duration: 10
+  },
+  {
+    id: 444,
+    imageUrl: "./logo.png",
+    name: "React Router",
+    category: "Front-end",
+    description: "Curso de ReactJS",
+    duration: 6
+  },
+  {
+    id: 555,
+    imageUrl: "./logo.png",
+    name: "Context API",
+    category: "Back-end",
+    description: "Curso de React com TS",
+    duration: 10
+  },
+  {
+    id: 666,
+    imageUrl: "./logo.png",
+    name: "ReactJS",
+    category: "Front-end",
+    description: "Curso de ReactJS",
+    duration: 6
+  },
+  {
+    id: 777,
+    imageUrl: "./logo.png",
+    name: "React com TS",
+    category: "Front-end",
+    description: "Curso de React com TS",
+    duration: 10
+  },
+  {
+    id: 888,
+    imageUrl: "./logo.png",
+    name: "React Router",
+    category: "Front-end",
+    description: "Curso de ReactJS",
+    duration: 6
+  },
+  {
+    id: 999,
+    imageUrl: "./logo.png",
+    name: "Context API",
+    category: "Back-end",
+    description: "Curso de React com TS",
+    duration: 10
   }
 ];
 
